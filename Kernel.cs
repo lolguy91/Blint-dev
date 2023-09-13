286c819636691fadb4b259d8bde0ea119cbb2643
Windows 98 Guy <129888800+Windows-1998-Guy@users.noreply.github.com>
2023-09-13 23:05:51 +0300

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace Blint
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static string version = "0.0.12923";
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine($"Blint version {version}");
+        }
+
+        protected override void Run()
+        {
+            Random random = new Random();
+            var userGenerator = @$"Session{random.Next(1000, 9999)}";
+            Console.Write(@$"
+@-[ BLINT SESSION ]
+|
+@-[{userGenerator}]-> ");
+            string read = Console.ReadLine();
+
+            if (read == "getneon")
+            {
+                getNeon();
+            }
+
+            if (read == "poweroff")
+            {
+                Cosmos.System.Power.Shutdown();
+            }
+        }
+
+        static void getNeon()
+        {
+            Neon82.Neon82.GetNeon();
+        }
+    }
+}
---
+++ neon82.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Blint;
+
+namespace Neon82
+{
+    public class Neon82
+    {
+        public static void GetNeon()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("###########   ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"Blint version {Blint.Kernel.version}\n");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("  ####   ###");
+            Console.WriteLine("    ##   ###");
+            Console.WriteLine("    #######");
+            Console.WriteLine("    ##   ###");
+            Console.WriteLine("    ##   ###");
+            Console.WriteLine("    #######");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ResetColor();
+        }
+    }
+}
